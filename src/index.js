89f76ca45c0f76a27c720a22f0029a0064a617d7
Oleg Borovikov <hhyahhh@gmail.com>
2021-03-15 17:20:56 +0300

    reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let b = 0;
+    n = Math.abs(n);
+    while (n > 0) {
+        b += n % 10;
+        b *= 10;
+        n = Math.floor(n / 10);
+    }
+    b = Math.floor(b / 10);
+    return (b);
 }
